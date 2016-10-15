f61bececde5b95ff9632c57b05f5b017885aaab5
thewalla07 <thewalla07@gmail.com>
2016-10-15 17:29:49 +0100

    Fixed bad tabs.

--- Assignment1.java
+++ Assignment1.java
@@ -2,12 +2,12 @@ import java.math.BigInteger;
 
 public class Assignment1 {
 
-	public static void main(String[] args) {
-        
-		System.out.println("Hello, Crypto!");
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Crypto!");
 
         System.out.println(createNBitKey(4));
-	}
+    }
     
     public static BigInteger createNBitKey(long n) {
         return new BigInteger(n);
