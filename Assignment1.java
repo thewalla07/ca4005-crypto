2703595e4fee3033fad940fe2731b8721e0d1417
thewalla07 <thewalla07@gmail.com>
2016-10-16 17:22:38 +0100

    Added n-bit ey generator

--- Assignment1.java
+++ Assignment1.java
@@ -1,15 +1,19 @@
 import java.math.BigInteger;
+import java.util.Random;
 
 public class Assignment1 {
+
+    private static int nBits = 1023;
 
     public static void main(String[] args) {
 
         System.out.println("Hello, Crypto!");
 
-        System.out.println(createNBitKey(4));
+        System.out.println("Private Key of " + nBits + " bits: " + createNBitKey(nBits));
     }
     
-    public static BigInteger createNBitKey(long n) {
-        return new BigInteger(n);
+    public static BigInteger createNBitKey(int nBits) {
+
+        return new BigInteger(nBits, new Random());
     }
 }
